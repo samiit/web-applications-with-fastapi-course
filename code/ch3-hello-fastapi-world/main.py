96231102f42a5cf075744255a3fec293e85a3188
Sam <sam.cfd.iitm@gmail.com>
2022-11-15 18:02:45 +0530

    Blacked the file

--- code/ch3-hello-fastapi-world/main.py
+++ code/ch3-hello-fastapi-world/main.py
@@ -4,9 +4,11 @@ import uvicorn
 
 app = fastapi.FastAPI()
 
+
 @app.get(path="/")
 def index():
     return JSONResponse(content={"message": "Hello FastAPI!"})
 
-if __name__ == 'main':
+
+if __name__ == "main":
     uvicorn.run(app)
