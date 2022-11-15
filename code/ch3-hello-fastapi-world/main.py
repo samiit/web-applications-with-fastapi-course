30b5b395cfb36765277f48a1e93c02964c2af238
Sam <sam.cfd.iitm@gmail.com>
2022-11-15 17:55:25 +0530

    Script vs Server run

--- code/ch3-hello-fastapi-world/main.py
+++ code/ch3-hello-fastapi-world/main.py
@@ -1,8 +1,12 @@
 import fastapi
 from fastapi.responses import JSONResponse
+import uvicorn
 
 app = fastapi.FastAPI()
 
 @app.get(path="/")
 def index():
     return JSONResponse(content={"message": "Hello FastAPI!"})
+
+if __name__ == 'main':
+    uvicorn.run(app)
