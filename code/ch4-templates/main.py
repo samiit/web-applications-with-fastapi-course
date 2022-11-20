a965fb20bd856e1d96f2fedad6a1e122a509ad3a
Sam <sam.cfd.iitm@gmail.com>
2022-11-20 20:24:37 +0530

    First HTML template response

--- code/ch4-templates/main.py
+++ code/ch4-templates/main.py
@@ -1,24 +1,21 @@
 import fastapi
+import fastapi_chameleon
 import uvicorn
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+from fastapi_chameleon import template
 
 app = fastapi.FastAPI()
 
+fastapi_chameleon.global_init("templates")
+
 
 @app.get(path="/")
+@template(template_file="index.html")
 def index():
-    content = """
-    <h1>First HTML response</h1>
-    <div>Hello FastAPI course! Now we're getting into HTML responses with templating!</div>
-    <p> There are three templating frameworks we'll assess: </p>
-    <ol>
-        <li><a href="https://jinja.palletsprojects.com/en/3.1.x/" target="_" rel="noopener noreferrer">Jinja2</a></li>
-        <li><a href="https://www.makotemplates.org/" target="_" rel="noopener noreferrer">Mako</a></li>
-        <li><a href="https://chameleon.readthedocs.io/en/latest/" target="_" rel="noopener noreferrer">Chameleon</a></li>
-    </ol>
-    And the winner for this course is Chameleon.
-    """
-    return HTMLResponse(content=content)
+    user_name = "smathew"
+    content = {"user_name": user_name}
+    # return JSONResponse(content={"user_name": user_name})
+    return content
 
 
 if __name__ == "__main__":
