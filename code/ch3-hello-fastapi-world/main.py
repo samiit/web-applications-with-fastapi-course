cedc6e7c322225b74378f1d6845eb11a47176849
Sam <sam.cfd.iitm@gmail.com>
2022-11-20 19:40:42 +0530

    Fixed the issue

--- code/ch3-hello-fastapi-world/main.py
+++ code/ch3-hello-fastapi-world/main.py
@@ -10,5 +10,5 @@ def index():
     return JSONResponse(content={"message": "Hello FastAPI course!"})
 
 
-# if __name__ == "main":
-#     uvicorn.run(app)
+if __name__ == "__main__":
+    uvicorn.run(app)
