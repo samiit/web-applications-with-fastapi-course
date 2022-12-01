7053df70198488983ca15c1c5138998cdca950e1
Sam <sam.cfd.iitm@gmail.com>
2022-12-02 03:33:59 +0530

    Skip explicit template file; keep structure rather

--- code/ch4-templates/views/home.py
+++ code/ch4-templates/views/home.py
@@ -5,7 +5,7 @@ router = fastapi.APIRouter()
 
 
 @router.get(path="/")
-@template(template_file="home/index.pt")
+@template()  # you may as well skip mentioning the `template_file`, if folder structure maintained
 def index(user: str = "anon"):
     content = {"user_name": user.title()}
     return content
